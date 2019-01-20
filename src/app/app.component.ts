ac9e3758eaf5ac896be5af23f8cb7b76c95ba6eb
Sourec <tothemyers@gmail.com>
2019-01-20 14:00:22 +0200

    Add embedded google map!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  map: any;
+
+  ngOnInit(): void {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 29.554237, lng: 34.954729},
+      zoom: 14.5,
+      gestureHandling: 'none',
+      zoomControl: false
+    });
+  }
 }
